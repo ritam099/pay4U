c17f2203488260f9f3be823044461cf75a802f3f
ritam099 <ritam.maity@mca.christuniversity.in>
2024-07-27 23:16:57 +0530

    Added more content

---
+++ app.js
@@ -0,0 +1,33 @@
+const slides = document.querySelectorAll(".slide")
+var counter = 0;
+
+// console.log(slides)
+
+slides.forEach(
+    (slide, index) => {
+        slide.style.left = `${index * 100}%`
+    }
+)
+
+const goNext = () => {
+    if (counter < slides.length - 1) {
+    counter++;
+    slideImage();
+    }
+};
+
+const goPrev = () => {
+    if (counter != 0) {
+    counter--;
+    slideImage();
+    }
+};
+
+const slideImage = () => {
+    slides.forEach(
+        (slide) => {
+            slide.style.transform = `translateX(-${counter * 100}%)`
+        }
+    )
+}
+
